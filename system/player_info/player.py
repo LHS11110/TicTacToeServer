7b2811a857a4b3cdb83f368e9263e286f211d2fd
LHS11110 <81010297+LHS11110@users.noreply.github.com>
2023-03-24 10:12:43 +0900

    Create player.py

---
+++ system/player_info/player.py
@@ -0,0 +1,32 @@
+from socket import socket
+from queue import Queue
+from json import loads
+from typing import Any
+import traceback
+
+
+class Player:
+    def __init__(self, sock: socket) -> None:
+        self.sock: socket = sock
+        self.match_check: bool = False
+
+    @property
+    def state(self) -> bool:
+        try:
+            self.sock.send("".encode())
+        except:
+            return False
+        return True
+
+    def load(self, command_q: Queue[Any]) -> None:
+        try:
+            buff: str = self.sock.recv(2048).decode()
+            for data in buff.split("\\"):
+                if len(data) == 0:
+                    continue
+                try:
+                    command_q.put(loads(data))
+                except:
+                    print(traceback.format_exc(), end="Invalid Data Type Conversion\n")
+        except:
+            ...
