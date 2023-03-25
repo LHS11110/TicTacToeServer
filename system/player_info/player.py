08e6f1f20da0f68b352beda5f6ccf9114f5bf56c
LHS11110 <81010297+LHS11110@users.noreply.github.com>
2023-03-25 12:53:02 +0900

    Update player.py

--- system/player_info/player.py
+++ system/player_info/player.py
@@ -1,8 +1,9 @@
-from socket import socket
+from socket import socket, error as SocketError
 from queue import Queue
 from json import loads
 from typing import Any
 import traceback
+import errno
 
 
 class Player:
@@ -11,23 +12,38 @@ class Player:
         self.match_check: bool = False
         self.address: Any = address
         self.name: str = ""
+        self.buffs: list[str] = []
 
     @property
     def state(self) -> bool:
         try:
-            self.sock.send("".encode())
-        except:
-            return False
+            msg: bytes = b""
+            msg = self.sock.recv(2048)
+            for m in msg.decode().split("\\"):
+                if len(m) == 0:
+                    continue
+                self.buffs.append(m)
+            if len(msg) == 0:
+                return False
+        except SocketError as error:
+            if error.errno == errno.WSAECONNRESET:
+                return False
         return True
 
     def load(self, command_q: Queue[Any]) -> None:
         try:
             buff: str = self.sock.recv(2048).decode()
+            while len(self.buffs) != 0:
+                try:
+                    command_q.put(loads(self.buffs.pop()))
+                except:
+                    print(traceback.format_exc(), end="Invalid Data Type Conversion\n")
             for data in buff.split("\\"):
                 if len(data) == 0:
                     continue
                 try:
-                    command_q.put(loads(data))
+                    command: Any = loads(data)
+                    command_q.put(command)
                 except:
                     print(traceback.format_exc(), end="Invalid Data Type Conversion\n")
         except:
